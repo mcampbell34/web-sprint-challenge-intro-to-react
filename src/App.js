7c5fbbdb4bce3f782e98eb874b0173e4371827cb
mcampbell34 <mcampbell@fastly.com@fastly.com>
2021-01-09 10:38:01 -0800

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
 import './App.css';
+import styled from 'styled-components'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  const [characterDetails, setCharacterDetails] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/?')
+    .then((res) => {
+      console.log(res.data.results); 
+      setCharacterDetails(res.data.results);
+    })
+    .catch((err) => {
+      console.log("err");
+    })
+  }, [])
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -12,6 +31,11 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {
+        characterDetails.map(ch =>{
+          return <Character key={ch.id} info={ch} />
+        })
+      }
     </div>
   );
 }
